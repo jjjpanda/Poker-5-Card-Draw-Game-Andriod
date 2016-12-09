3caaed4df4daefcab89c12f486dd26d4214cd48e
jjjpanda <15862136+jjjpanda@users.noreply.github.com>
2016-12-08 19:08:03 -0500

    Empty Stuff,; blank slates

--- app/src/main/java/orangeboat/poker_ai/Players/Smart.java
+++ app/src/main/java/orangeboat/poker_ai/Players/Smart.java
@@ -1,8 +1,52 @@
 package orangeboat.poker_ai.Players;
 
+import orangeboat.poker_ai.Card;
+
 /**
  * Created by jawpa on 12/6/2016.
  */
 //High Roller
 public class Smart extends AI{
+
+    public Smart() {
+        super();
+        //change values
+        super.bluffRate = 0;
+        super.responseTime = 0;
+        super.shortStackPoint = 0;
+    }
+    public void update(int gameRound, int dealerPosition){
+        if (gameRound == 0) {
+            //preflop
+            a.deal('T', 'H');
+            b.deal('9', 'S');
+            if( evaluateCards(a,b, gameRound, money) ){
+                //do action
+            }
+        }
+        else if( gameRound == 1){
+            //flop
+        }
+        else if( gameRound == 2){
+            //turn
+        }
+        else if( gameRound == 3){
+            //river
+        }
+    }
+    public boolean evaluateCards(Card a, Card b, int gameRound, int money){
+        if (gameRound == 0) {
+            //preflop
+        }
+        else if( gameRound == 1){
+            //flop
+        }
+        else if( gameRound == 2){
+            //turn
+        }
+        else if( gameRound == 3){
+            //river
+        }
+        return true;
+    }
 }
